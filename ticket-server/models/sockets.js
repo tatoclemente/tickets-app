f5c55078bdb7c3ec5dbbef44e34f31e14606c8ea
tatoclemente <tatoapuros@gmail.com>
2024-01-20 19:44:43 -0300

    Comunication with the client

--- ticket-server/models/sockets.js
+++ ticket-server/models/sockets.js
@@ -1,10 +1,14 @@
 
+const TicketList = require('./ticket-list')
 
 class Sockets {
 
   constructor( io ) {
 
     this.io = io;
+
+    // Crear la instancia de nuestro ticketlist
+    this.ticketList = new TicketList();
 
     this.socketsEventes();
     
@@ -15,13 +19,21 @@ class Sockets {
     // On connection
     this.io.on('connection', ( socket ) => { 
 
+      console.log('Cliente conectado');
       // Escuchar evento: mensaje-to-server
-      socket.on('mensaje-to-server', (data) => {
-        console.log(data);
 
-        // Emitir evento: mensaje-from-server
-        this.io.emit('mensaje-from-server', data)
+      socket.on('select-ticket', ( _, callback ) => {
+        const newTicket = this.ticketList.createTicket()
+        callback(newTicket)
+        
       })
+
+      // socket.on('mensaje-to-server', (data) => {
+      //   console.log(data);
+
+      //   // Emitir evento: mensaje-from-server
+      //   this.io.emit('mensaje-from-server', data)
+      // })
       
     }); 
   }
